8f48af853b5e6af0a49f1e74606eba1042447297
Hendrix Gullixson <hgullixson@gmail.com>
2025-04-02 15:45:25 -0500

    fixed "city" bug

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -1,23 +1,22 @@
 'use client';
-
-import { getLocationOrigin } from "next/dist/shared/lib/utils"
 import { useEffect, useState } from "react"
 
 export default function LocationFinderServer() {
-    const [locationInfo, setLocationInfo] = useState({})
+    const [locationInfo, setLocationInfo] = useState({City: "N/A"})
 
     const getLocationInfo = async () => {
-    const response = await fetch('https://apip.cc/json')
+    const response = await fetch('https://apip.cc/json');
     const locationData = await response.json();
     setLocationInfo(locationData);
     }
     
     useEffect(() => {
         getLocationInfo();
-    }, [])
+    })
+
     return (
         <>
-        <h1>Hello from {locationInfo?.city} - client component</h1>
+        <h1>Hello from {locationInfo.City} - client component</h1>
         </>
     )
 
